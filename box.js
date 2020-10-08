2765cef8bc49b2a18de4516715cfb9806db83ca1
nikunjsharm <71394373+nikunjsharm@users.noreply.github.com>
2020-10-08 14:08:49 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,11 @@
+class box extends baseclass{
+     constructor(x,y,width,height){
+         
+        super(x,y,width,height)
+        this.image=loadImage("sprites/wood1.png")
+      
+     }
+    
+      
+ };
+ 
